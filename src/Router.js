e8b57da3fcf1455d5dc867b10b600991d22c5d4d
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2020-01-14 19:23:45 +0545

    Add basic routes

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 
 function Router() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <div className="flex bg-gray-100">
+        <div className="w-1/5 bg-gray-200 border-solid border-r border-gray-300 h-screen">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="w-4/5">
+          <Switch>
+            <Route path="/about">
+              <div>About</div>
+            </Route>
+            <Route path="/users">
+              <div>Users</div>
+            </Route>
+            <Route path="/">
+              <div>Home</div>
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default Router;
