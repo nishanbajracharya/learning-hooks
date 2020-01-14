41282f03182fdb8453551c5d35a3799bf72ba2e9
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2020-01-14 20:42:33 +0545

    Add sidebar styles

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
 
 import routes from './constants/routes';
 import UseState from './views/useState';
@@ -9,18 +9,18 @@ function Router() {
   return (
     <BrowserRouter>
       <div className="flex bg-gray-100">
-        <div className="w-1/5 bg-gray-200 border-solid border-r border-gray-300 h-screen">
+        <div className="w-1/6 border-r border-gray-300 h-screen">
           <nav>
             <ul>
               {routes.links.map((route, index) => (
                 <li key={index}>
-                  <Link to={route.to}>{route.label}</Link>
+                  <NavLink to={route.to} className="block p-4 hover:bg-gray-300" activeClassName="bg-gray-400" exact>{route.label}</NavLink>
                 </li>
               ))}
             </ul>
           </nav>
         </div>
-        <div className="w-4/5">
+        <div className="w-5/6">
           <Switch>
             <Route path={routes.paths.USE_STATE}>
               <UseState />
--- src/components/Viewer.js
+++ src/components/Viewer.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import { tomorrowNight } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { atomOneLight } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 import content from '../constants/content';
 
 function Viewer(props) {
   return (
-    <div>
-      <SyntaxHighlighter language="javascript" style={tomorrowNight}>
+    <div className="lg:clearfix">
+      <div className="lg:w-1/2 lg:float-left lg:h-screen">
+      <SyntaxHighlighter language="javascript" style={atomOneLight} className="h-full border-r">
         {content[props.path]}
       </SyntaxHighlighter>
-      {props.children}
+      </div>
+      <div className="lg:w-1/2 lg:float-left lg:h-screen bg-white">{props.children}</div>
     </div>
   );
 }
